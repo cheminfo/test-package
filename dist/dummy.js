40e65ed58137d7ecb3474444961ee22671b21394
Michaël Zasso <mic.besace@gmail.com>
2015-03-05 15:21:03 +0100

    Release v4.2.10

--- dist/dummy.js
+++ dist/dummy.js
@@ -1,6 +1,6 @@
 /**
  * dummy - Dummy project - used as a starting point for new libraries
- * @version v4.2.9
+ * @version v4.2.10
  * @link https://github.com/cheminfo-js/dummy
  * @license MIT
  */

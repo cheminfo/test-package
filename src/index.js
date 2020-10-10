985bd4e3d8a86c45a5be00acd9dda28da266ab3d
Michaël Zasso <targos@protonmail.com>
2020-10-10 13:25:13 +0200

    chore: add jsdoc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+/**
+ * Returns a very important number
+ * @returns {number}
+ */
 export default function theDefaultExport() {
   return 42;
 }
 
+/**
+ * Value that can be incremented
+ * @type {number}
+ */
 export let value = 0;
 
+/**
+ * Increments the value by one.
+ */
 export function incrementValue() {
   value++;
 }

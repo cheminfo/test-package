4b4c0dbcd4e315aee792811bce23e26b8d03883c
Michaël Zasso <targos@protonmail.com>
2026-02-16 14:18:37 +0100

    chore: update dependencies

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,17 +1,22 @@
 import { describe, expect, it } from 'vitest';
 
-import testLib, { value, incrementValue } from '../index.js';
+import testLib, { incrementValue, value } from '../index.js';
 
 describe('testing the module', () => {
   it('should return 42', () => {
     expect(testLib()).toBe(42);
   });
+
   it('should increment', () => {
     expect(value).toBe(0);
+
     incrementValue();
+
     expect(value).toBe(1);
+
     incrementValue();
     incrementValue();
+
     expect(value).toBe(3);
   });
 });
